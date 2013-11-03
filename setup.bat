527e11c65edda629e60530275d184c55db2d76bb
Rayner Lucas <git@magic-cookie.co.uk>
2013-11-03 01:19:50 +0000

    Add install scripts

---
+++ setup.bat
@@ -0,0 +1,7 @@
+@echo off
+REM   Setup script for neuroConstruct, to allow one-click setup instead
+REM   of using the command line.
+
+cd neuroConstruct
+nC.bat -make
+@echo on
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd neuroConstruct
+./nC.sh -make
